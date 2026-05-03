ff01033ca020e05ef1b2ff03ac8f1821753de285
Mark Gerald Pacun <markgpacun@gmail.com>
2026-05-03 16:33:47 +0800

    Update CORS for production

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://qcu-coop.vercel.app',
+  ],
+  credentials: true,
+}));
 app.use(express.json());
 
 // Connect the system to MongoDB
